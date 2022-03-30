7dc4dd0b8f924441c222af5ab7e8eb5988897561
Nils Quetschlich <Nils.Quetschlich@tum.de>
2022-03-30 14:59:22 +0200

    ✨ added OQC's Lucy Quantum Computer to both compilers and added different optimization levels for tket

--- evaluator/pytket_plugin.py
+++ evaluator/pytket_plugin.py
@@ -7,25 +7,52 @@ from pytket.passes import PlacementPass, RoutingPass, FullPeepholeOptimise, Synt
 from pytket.placement import LinePlacement
 
 from pytket import qasm
-from evaluator.utils import calc_score_from_str, calc_score, get_cmap_rigetti_m1, get_cmap_imbq_washington, get_ibm_washington, get_rigetti_m1, get_ionq
+from evaluator.utils import *
 import pytket
 from pytket import OpType, _tket, Circuit
 from pytket.passes import RebaseCustom
+from pytket.passes._decompositions import _TK1_to_X_SX_Rz
+from pytket.extensions.qiskit import tk_to_qiskit
+
+from qiskit.test.mock import FakeMontreal
 
 
 def get_tket_scores(qasm_qc, opt_level=0):
-    qc = qasm.circuit_from_qasm_str(qasm_qc)
-    score_ibm = get_ibm_score(qc, opt_level)
+    penalty_width = 100000
 
     qc = qasm.circuit_from_qasm_str(qasm_qc)
-    score_ionq = get_ionq_score(qc)
+    if qc.n_qubits > get_ibm_washington()['num_qubits']:
+        score_ibm_washington = penalty_width
+    else:
+        score_ibm_washington = get_ibm_washington_score(qc, opt_level)
 
     qc = qasm.circuit_from_qasm_str(qasm_qc)
-    score_rigetti = get_rigetti_score(qc, opt_level)
+    if qc.n_qubits > get_ibm_montreal()['num_qubits']:
+        score_ibm_montreal = penalty_width
+    else:
+        score_ibm_montreal = get_ibm_montreal_score(qc, opt_level)
 
-    #print("Scores: ", [score_ibm, score_ionq, score_rigetti])
+    qc = qasm.circuit_from_qasm_str(qasm_qc)
+    if qc.n_qubits > get_ionq()['num_qubits']:
+        score_ionq = penalty_width
+    else:
+        score_ionq = get_ionq_score(qc, opt_level)
 
-    return [score_ibm, score_ionq, score_rigetti]
+    qc = qasm.circuit_from_qasm_str(qasm_qc)
+    if qc.n_qubits > get_rigetti_m1()['num_qubits']:
+        score_rigetti = penalty_width
+    else:
+        score_rigetti = get_rigetti_score(qc, opt_level)
+
+    qc = qasm.circuit_from_qasm_str(qasm_qc)
+    if qc.n_qubits > get_oqc_lucy()['num_qubits']:
+        score_oqc = penalty_width
+    else:
+        score_oqc = get_oqc_score(qc, opt_level)
+
+    print("Scores TKET: ", [score_ibm_washington, score_ibm_montreal, score_ionq, score_rigetti, score_oqc])
+
+    return [score_ibm_washington, score_ibm_montreal, score_ionq, score_rigetti, score_oqc]
 
 
 def get_rigetti_score(qc, opt_level):
@@ -39,47 +66,83 @@ def get_rigetti_score(qc, opt_level):
     elif opt_level == 2:
         FullPeepholeOptimise().apply(qc)
     backend.rebase_pass().apply(qc)
-    qc_qasm = qasm.circuit_to_qasm_str(qc)
     rigetti_m1 = get_rigetti_m1()
-    score_rigetti = calc_score_from_str(qc_qasm, rigetti_m1)
+    score_rigetti = calc_score(qc, rigetti_m1, "tket")
     return score_rigetti
 
 
-def get_ionq_score(qc):
+def get_ionq_score(qc, opt_level):
     #b3 = IonQBackend(api_key="")
     #b3.rebase_pass().apply(qasm_qc)
     ionq_rebase = get_ionq_rebase()
     ionq_rebase.apply(qc)
-    qc_qasm = qasm.circuit_to_qasm_str(qc)
+    if opt_level == 1:
+        SynthesiseTket().apply(qc)
+        ionq_rebase.apply(qc)
+    elif opt_level == 2:
+        FullPeepholeOptimise().apply(qc)
+        ionq_rebase.apply(qc)
     ionq = get_ionq()
-    score_ionq = calc_score_from_str(qc_qasm, ionq)
+    score_ionq = calc_score(qc, ionq, "tket")
+    return score_ionq
+
+def get_oqc_score(qc, opt_level):
+    oqc_rebase = get_oqc_rebase()
+    oqc_rebase.apply(qc)
+    if opt_level == 1:
+        SynthesiseTket().apply(qc)
+        oqc_rebase.apply(qc)
+    elif opt_level == 2:
+        FullPeepholeOptimise().apply(qc)
+        oqc_rebase.apply(qc)
+    oqc_lucy = get_oqc_lucy()
+    score_ionq = calc_score(qc, oqc_lucy, "tket")
     return score_ionq
 
 
-def get_aqt_score(qc):
+def get_aqt_score(qc, opt_level):
     backend = AQTBackend(device_name="sim/noise-model-1", access_token='')
     backend.rebase_pass().apply(qc)
-    qc_qasm = qasm.circuit_to_qasm_str(qc)
+    if opt_level == 1:
+        SynthesiseTket().apply(qc)
+        backend.rebase_pass().apply(qc)
+    elif opt_level == 2:
+        FullPeepholeOptimise().apply(qc)
+        backend.rebase_pass().apply(qc)
     ionq = get_ionq()
-    score_aqt = calc_score_from_str(qc_qasm, ionq)
+    score_aqt = calc_score(qc, ionq)
     return score_aqt
 
 
-def get_ibm_score(qc, opt_level):
-    ibm_arch = pytket.architecture.Architecture(get_cmap_imbq_washington())
+def get_ibm_washington_score(qc, opt_level):
+    ibm_washington_arch = pytket.architecture.Architecture(get_cmap_imbq_washington())
     backend = IBMQBackend("ibmq_santiago")
     backend.rebase_pass().apply(qc)
-    PlacementPass(LinePlacement(ibm_arch)).apply(qc)
-    RoutingPass(ibm_arch).apply(qc)
+    PlacementPass(LinePlacement(ibm_washington_arch)).apply(qc)
+    RoutingPass(ibm_washington_arch).apply(qc)
     if opt_level == 1:
         SynthesiseTket().apply(qc)
     elif opt_level == 2:
         FullPeepholeOptimise().apply(qc)
     backend.rebase_pass().apply(qc)
-    qc_qasm = qasm.circuit_to_qasm_str(qc)
     ibm_washington = get_ibm_washington()
-    score_ibm = calc_score_from_str(qc_qasm, ibm_washington)
-    return score_ibm
+    score_ibm_washington = calc_score(qc, ibm_washington, "tket")
+    return score_ibm_washington
+
+def get_ibm_montreal_score(qc, opt_level):
+    ibm_montreal_arch = pytket.architecture.Architecture(FakeMontreal().configuration().coupling_map)
+    backend = IBMQBackend("ibmq_santiago")
+    backend.rebase_pass().apply(qc)
+    PlacementPass(LinePlacement(ibm_montreal_arch)).apply(qc)
+    RoutingPass(ibm_montreal_arch).apply(qc)
+    if opt_level == 1:
+        SynthesiseTket().apply(qc)
+    elif opt_level == 2:
+        FullPeepholeOptimise().apply(qc)
+    backend.rebase_pass().apply(qc)
+    ibm_montreal = get_ibm_montreal()
+    score_ibm_montreal = calc_score(qc, ibm_montreal, "tket")
+    return score_ibm_montreal
 
 
 def get_ionq_rebase():
@@ -93,3 +156,12 @@ def get_ionq_rebase():
 
     ionq_rebase = RebaseCustom(ionq_gateset, cx_in_ionq, tk1_to_rzry)
     return ionq_rebase
+
+def get_oqc_rebase():
+    # ["rz", "sx", "x", "ecr"]
+    oqc_gateset = {OpType.Rz, OpType.SX, OpType.X, OpType.ECR}
+    cx_in_oqc = _tket.circuit._library._CX_using_ECR()
+    tk1_to_rz_x_sx = _TK1_to_X_SX_Rz
+
+    oqc_rebase = RebaseCustom(oqc_gateset, cx_in_oqc, tk1_to_rz_x_sx)
+    return oqc_rebase
--- evaluator/qiskit_plugin.py
+++ evaluator/qiskit_plugin.py
@@ -1,6 +1,6 @@
 from qiskit import transpile
 from evaluator.utils import *
-
+from qiskit.test.mock import FakeMontreal
 
 def get_aqt_gateset():
     from qiskit_aqt_provider import AQTProvider
@@ -13,16 +13,18 @@ def get_aqt_gateset():
 
 def get_qiskit_scores(qasm_qc, opt_level=0):
 
-    ibm_gates = ["id", "rz", "sx", "x", "cx", "reset"]
+    penalty_width = 100000
+
+    ibm_gates = ["rz", "sx", "x", "cx"]
     rigetti_gates = ["rx", "rz", "cz"]
     ionq_gates = ["rxx", "rz", "ry", "rx"]
-    oqc_gates = ["rz", "sx", "x", "cx"]
+    oqc_gates = ["rz", "sx", "x", "ecr"]
 
     qc = QuantumCircuit.from_qasm_str(qasm_qc)
 
     ibm_washington = get_ibm_washington()
     if qc.num_qubits > ibm_washington["num_qubits"]:
-        score_ibm_washington = 100000
+        score_ibm_washington = penalty_width
     else:
         qc_ibm = transpile(
             qc,
@@ -30,11 +32,11 @@ def get_qiskit_scores(qasm_qc, opt_level=0):
             optimization_level=opt_level,
             coupling_map=get_cmap_imbq_washington(),
         )
-        score_ibm_washington = calc_score_from_str(qc_ibm.qasm(), ibm_washington)
+        score_ibm_washington = calc_score(qc_ibm, ibm_washington, "qiskit")
 
     ibm_montreal = get_ibm_montreal()
     if qc.num_qubits > ibm_montreal["num_qubits"]:
-        score_ibm_montreal = 100000
+        score_ibm_montreal = penalty_width
     else:
         qc_ibm = transpile(
             qc,
@@ -42,18 +44,18 @@ def get_qiskit_scores(qasm_qc, opt_level=0):
             optimization_level=opt_level,
             coupling_map=FakeMontreal().configuration().coupling_map,
         )
-        score_ibm_montreal = calc_score_from_str(qc_ibm.qasm(), ibm_washington)
+        score_ibm_montreal = calc_score(qc_ibm, ibm_washington, "qiskit")
 
     ionq = get_ionq()
     if qc.num_qubits > ionq["num_qubits"]:
-        score_ionq = 100000
+        score_ionq = penalty_width
     else:
         qc_ion = transpile(qc, basis_gates=ionq_gates, optimization_level=opt_level)
-        score_ionq = calc_score_from_str(qc_ion.qasm(), ionq)
+        score_ionq = calc_score(qc_ion, ionq, "qiskit")
 
     rigetti_m1 = get_rigetti_m1()
     if qc.num_qubits > rigetti_m1["num_qubits"]:
-        score_rigetti = 100000
+        score_rigetti = penalty_width
     else:
         qc_rigetti = transpile(
             qc,
@@ -61,11 +63,11 @@ def get_qiskit_scores(qasm_qc, opt_level=0):
             optimization_level=opt_level,
             coupling_map=get_cmap_rigetti_m1(10),
         )
-        score_rigetti = calc_score_from_str(qc_rigetti.qasm(), rigetti_m1)
+        score_rigetti = calc_score(qc_rigetti, rigetti_m1, "qiskit")
 
     oqc_lucy = get_oqc_lucy()
     if qc.num_qubits > oqc_lucy["num_qubits"]:
-        score_oqc = 100000
+        score_oqc = penalty_width
     else:
         qc_oqc = transpile(
             qc,
@@ -73,11 +75,11 @@ def get_qiskit_scores(qasm_qc, opt_level=0):
             optimization_level=opt_level,
             coupling_map=get_c_map_oqc_lucy(),
         )
-        score_oqc = calc_score_from_str(qc_oqc.qasm(), oqc_lucy)
+        score_oqc = calc_score(qc_oqc, oqc_lucy, "qiskit")
 
     # print("Scores: ", [score_ibm_washington, score_ionq, score_rigetti])
 
-    print(
+    print("Scores qiskit: ",
         [score_ibm_washington, score_ibm_montreal, score_ionq, score_rigetti, score_oqc]
     )
     return [
