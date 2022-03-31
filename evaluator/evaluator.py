289b31b6ab494c35a9a6190780907d2458902238
Nils Quetschlich <Nils.Quetschlich@tum.de>
2022-03-31 17:18:41 +0200

    ✨both qiskit and tket now take the a quantumcircuit resp. circuit object now instead of a qasm file

--- evaluator/evaluator.py
+++ evaluator/evaluator.py
@@ -4,9 +4,12 @@ from pytket_plugin import *
 from utils import get_openqasm_gates
 from mqt.bench import benchmark_generator
 
+from pytket.extensions.qiskit import qiskit_to_tk
+
 import numpy as np
 import argparse
 from datetime import datetime
+
 
 def dict_to_featurevector(gate_dict):
     openqasm_gates_list = get_openqasm_gates()
@@ -19,24 +22,53 @@ def dict_to_featurevector(gate_dict):
             res_dct[key] = val
     return res_dct
 
-def create_training_data(min_qubit:int, max_qubit:int, stepsize:int = 1):
-    benchmarks = ["dj", "grover-noancilla", "grover-v-chain", "ghz", "graphstate", "qft", "qftentangled",
-                  "qpeexact", "qpeinexact", "qwalk-noancilla", "qwalk-v-chain", "realamprandom", "su2random",
-                  "twolocalrandom", "vqe", "wstate"]
+
+def create_training_data(min_qubit: int, max_qubit: int, stepsize: int = 1):
+    benchmarks = [
+        "dj",
+        "grover-noancilla",
+        "grover-v-chain",
+        "ghz",
+        "graphstate",
+        "qft",
+        "qftentangled",
+        "qpeexact",
+        "qpeinexact",
+        "qwalk-noancilla",
+        "qwalk-v-chain",
+        "realamprandom",
+        "su2random",
+        "twolocalrandom",
+        "vqe",
+        "wstate",
+    ]
     res = []
     for benchmark in benchmarks:
         for num_qubits in range(min_qubit, max_qubit, stepsize):
-            if "noancilla" in benchmark and num_qubits>12 or "v-chain" in benchmark and num_qubits>12:
+            if (
+                "noancilla" in benchmark
+                and num_qubits > 12
+                or "v-chain" in benchmark
+                and num_qubits > 12
+            ):
                 break
             print(benchmark, num_qubits)
             qc = benchmark_generator.get_one_benchmark(benchmark, 1, num_qubits)
+
             qasm_qc = qc.qasm()
-            ops_list = qc.count_ops()
+            qc = QuantumCircuit.from_qasm_str(qasm_qc)
+            qiskit_score = get_qiskit_scores(qc)
             try:
-                best_arch = np.argmin(get_tket_scores(qasm_qc) + get_qiskit_scores(qasm_qc))
+                qc_tket = qiskit_to_tk(qc)
+                ops_list = qc.count_ops()
+                tket_scores = get_tket_scores(qc_tket)
+                best_arch = np.argmin(tket_scores + qiskit_score)
                 res.append((ops_list, best_arch, num_qubits))
             except Exception as e:
                 print("fail: ", e)
+                # qc_tket = qasm.circuit_from_qasm_str(qc.qasm())
+                # tket_scores = get_tket_scores(qc_tket)
+        # else:
 
     training_data = []
     for elem in res:
@@ -45,14 +77,13 @@ def create_training_data(min_qubit:int, max_qubit:int, stepsize:int = 1):
         training_data.append((list(tmp.values()), elem[1]))
 
     x_train, y_train = zip(*training_data)
-    current_date = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
-    with open(current_date + '_X_train.csv', 'w') as FOUT:
+    current_date = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    with open(current_date + "_X_train.csv", "w") as FOUT:
         np.savetxt(FOUT, x_train)
-    with open(current_date + '_y_train.csv', 'w') as FOUT:
+    with open(current_date + "_y_train.csv", "w") as FOUT:
         np.savetxt(FOUT, y_train)
 
     return res
-
 
 
 if __name__ == "__main__":
@@ -64,9 +95,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--max", type=int, default=20,
     )
-    parser.add_argument(
-        "--step", type=int, default=3
-    )
+    parser.add_argument("--step", type=int, default=3)
 
     args = parser.parse_args()
     characteristics = create_training_data(args.min, args.max, args.step)
