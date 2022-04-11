126f61173358664198d7b20a2bccaee3f2a5b924
Nils Quetschlich <Nils.Quetschlich@tum.de>
2022-04-11 17:24:38 +0200

    ✨ added linter workflow

--- evaluator/tests/test_comp_flow.py
+++ evaluator/tests/test_comp_flow.py
@@ -4,11 +4,13 @@ import pytest
 from pytket.extensions.qiskit import qiskit_to_tk
 from mqt.bench import get_one_benchmark
 
+
 def test_qiskit_native_gatesets():
     assert qiskit_plugin.get_ibm_native_gates() == ["rz", "sx", "x", "cx"]
     assert qiskit_plugin.get_rigetti_native_gates() == ["rx", "rz", "cz"]
     assert qiskit_plugin.get_ionq_native_gates() == ["rxx", "rz", "ry", "rx"]
     assert qiskit_plugin.get_oqc_native_gates() == ["rz", "sx", "x", "ecr"]
+
 
 def test_qiskit_gate_counts():
     qc = get_one_benchmark("dj", 1, 3)
@@ -34,6 +36,7 @@ def test_qiskit_gate_counts():
     assert not get_rigetti_gates is None
     get_oqc_gates = qiskit_plugin.get_oqc_gates(qc, 0)
     assert get_oqc_gates is None
+
 
 def test_tket_gate_counts():
     qc = get_one_benchmark("dj", 1, 3)
@@ -62,11 +65,13 @@ def test_tket_gate_counts():
     get_oqc_gates = pytket_plugin.get_oqc_gates(qc_tket, 0)
     assert get_oqc_gates is None
 
+
 def test_get_qiskit_gates():
     qc = get_one_benchmark("ghz", 1, 5)
     res = qiskit_plugin.get_qiskit_gates(qc)
     assert res[0] == "qiskit"
     assert len(res[1]) == 5
+
 
 def test_get_tket_gates():
     qc = get_one_benchmark("ghz", 1, 5)
