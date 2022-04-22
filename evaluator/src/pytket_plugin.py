5928a6631113d2fe764aa231562e32a91838efcd
Nils Quetschlich <Nils.Quetschlich@tum.de>
2022-04-22 14:11:27 +0200

    ✨changed the default pass such that the opt level is not parameterizable anymore

--- evaluator/src/pytket_plugin.py
+++ evaluator/src/pytket_plugin.py
@@ -3,22 +3,25 @@ from pytket.passes import (
     RoutingPass,
     FullPeepholeOptimise,
     SynthesiseTket,
+    DefaultMappingPass,
+    auto_rebase_pass,
 )
-from pytket.placement import LinePlacement
+from pytket.placement import LinePlacement, GraphPlacement
+
 from pytket import architecture
-from pytket.passes import auto_rebase_pass
+
 
 from qiskit.test.mock import FakeMontreal, FakeWashington
 from evaluator.src.utils import *
 
 
-def get_tket_gates(qc, opt_level=0):
+def get_tket_gates(qc):
 
-    gates_ibm_washington = get_ibm_washington_gates(qc, opt_level)
-    gates_ibm_montreal = get_ibm_montreal_gates(qc, opt_level)
-    gates_ionq = get_ionq_gates(qc, opt_level)
-    gates_rigetti = get_rigetti_gates(qc, opt_level)
-    gates_oqc = get_oqc_gates(qc, opt_level)
+    gates_ibm_washington = get_ibm_washington_gates(qc)
+    gates_ibm_montreal = get_ibm_montreal_gates(qc)
+    gates_ionq = get_ionq_gates(qc)
+    gates_rigetti = get_rigetti_gates(qc)
+    gates_oqc = get_oqc_gates(qc)
 
     return (
         "tket",
@@ -32,20 +35,18 @@ def get_tket_gates(qc, opt_level=0):
     )
 
 
-def get_rigetti_gates(qc, opt_level):
+def get_rigetti_gates(qc):
     if qc.n_qubits > get_rigetti_m1()["num_qubits"]:
         gates_rigetti = None
     else:
         backend = get_rigetti_rebase()
         rigetti_arch = architecture.Architecture(get_cmap_rigetti_m1(10))
         backend.apply(qc)
-        PlacementPass(LinePlacement(rigetti_arch)).apply(qc)
-        RoutingPass(rigetti_arch).apply(qc)
-        if opt_level == 1:
-            SynthesiseTket().apply(qc)
-        elif opt_level == 2:
-            FullPeepholeOptimise().apply(qc)
+
+        FullPeepholeOptimise().apply(qc)
+        DefaultMappingPass(rigetti_arch).apply(qc)
         backend.apply(qc)
+
         gates_rigetti = count_qubit_gates_tket(qc, "rigetti")
         assert sum(gates_rigetti) == qc.n_gates - qc.n_gates_of_type(
             OpType.Measure
@@ -53,18 +54,15 @@ def get_rigetti_gates(qc, opt_level):
     return gates_rigetti
 
 
-def get_ionq_gates(qc, opt_level):
+def get_ionq_gates(qc):
     if qc.n_qubits > get_ionq()["num_qubits"]:
         gates_ionq = None
     else:
         ionq_rebase = get_ionq_rebase()
         ionq_rebase.apply(qc)
-        if opt_level == 1:
-            SynthesiseTket().apply(qc)
-            ionq_rebase.apply(qc)
-        elif opt_level == 2:
-            FullPeepholeOptimise().apply(qc)
-            ionq_rebase.apply(qc)
+
+        FullPeepholeOptimise().apply(qc)
+        ionq_rebase.apply(qc)
         gates_ionq = count_qubit_gates_tket(qc, "ionq")
         assert sum(gates_ionq) == qc.n_gates - qc.n_gates_of_type(
             OpType.Measure
@@ -72,18 +70,19 @@ def get_ionq_gates(qc, opt_level):
     return gates_ionq
 
 
-def get_oqc_gates(qc, opt_level):
+def get_oqc_gates(qc):
     if qc.n_qubits > get_oqc_lucy()["num_qubits"]:
         gates_oqc = None
     else:
         oqc_rebase = get_oqc_rebase()
         oqc_rebase.apply(qc)
-        if opt_level == 1:
-            SynthesiseTket().apply(qc)
-            oqc_rebase.apply(qc)
-        elif opt_level == 2:
-            FullPeepholeOptimise().apply(qc)
-            oqc_rebase.apply(qc)
+
+        oqc_arch = architecture.Architecture(get_c_map_oqc_lucy())
+
+        FullPeepholeOptimise().apply(qc)
+        DefaultMappingPass(oqc_arch).apply(qc)
+
+        oqc_rebase.apply(qc)
         gates_oqc = count_qubit_gates_tket(qc, "oqc")
         assert sum(gates_oqc) == qc.n_gates - qc.n_gates_of_type(
             OpType.Measure
@@ -91,7 +90,7 @@ def get_oqc_gates(qc, opt_level):
     return gates_oqc
 
 
-def get_ibm_washington_gates(qc, opt_level):
+def get_ibm_washington_gates(qc):
     if qc.n_qubits > get_ibm_washington()["num_qubits"]:
         gates_ibm_washington = None
     else:
@@ -100,12 +99,10 @@ def get_ibm_washington_gates(qc, opt_level):
         )
         backend = get_ibm_rebase()
         backend.apply(qc)
-        PlacementPass(LinePlacement(ibm_washington_arch)).apply(qc)
-        RoutingPass(ibm_washington_arch).apply(qc)
-        if opt_level == 1:
-            SynthesiseTket().apply(qc)
-        elif opt_level == 2:
-            FullPeepholeOptimise().apply(qc)
+
+        FullPeepholeOptimise().apply(qc)
+        DefaultMappingPass(ibm_washington_arch).apply(qc)
+
         backend.apply(qc)
         gates_ibm_washington = count_qubit_gates_tket(qc, "ibm")
         assert sum(gates_ibm_washington) == qc.n_gates - qc.n_gates_of_type(
@@ -114,7 +111,7 @@ def get_ibm_washington_gates(qc, opt_level):
     return gates_ibm_washington
 
 
-def get_ibm_montreal_gates(qc, opt_level):
+def get_ibm_montreal_gates(qc):
     if qc.n_qubits > get_ibm_montreal()["num_qubits"]:
         gates_ibm_montreal = None
     else:
@@ -123,12 +120,9 @@ def get_ibm_montreal_gates(qc, opt_level):
         )
         backend = get_ibm_rebase()
         backend.apply(qc)
-        PlacementPass(LinePlacement(ibm_montreal_arch)).apply(qc)
-        RoutingPass(ibm_montreal_arch).apply(qc)
-        if opt_level == 1:
-            SynthesiseTket().apply(qc)
-        elif opt_level == 2:
-            FullPeepholeOptimise().apply(qc)
+
+        FullPeepholeOptimise().apply(qc)
+        DefaultMappingPass(ibm_montreal_arch).apply(qc)
         backend.apply(qc)
         gates_ibm_montreal = count_qubit_gates_tket(qc, "ibm")
         assert sum(gates_ibm_montreal) == qc.n_gates - qc.n_gates_of_type(
