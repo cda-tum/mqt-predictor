4194c5742309b8531270dc1f3661dd9bac16ddb3
Nils Quetschlich <Nils.Quetschlich@tum.de>
2022-05-18 16:14:26 +0200

    ✨ added postprocess function for faulty oqc qasm files

--- predictor/src/utils.py
+++ predictor/src/utils.py
@@ -4,6 +4,7 @@ import numpy as np
 from pytket import OpType
 from qiskit import QuantumCircuit
 import pandas as pd
+import os
 
 
 def get_width_penalty():
@@ -674,3 +675,49 @@ def get_rigetti_qubit_dict():
 
     assert len(mapping) == 80
     return mapping
+
+
+def postprocess_ocr_qasm_files(directory: str = "qasm_compiled"):
+
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        # checking if it is a file
+        if "oqc_qiskit" in f:
+            with open(f, "r") as f:
+                lines = f.readlines()
+            new_name = os.path.join("qasm_compiled_postprocessed", filename)
+            with open(new_name, "w") as f:
+                for line in lines:
+                    if not (
+                        "gate rzx" in line.strip("\n") or "gate ecr" in line.strip("\n")
+                    ):
+                        f.write(line)
+                    if "gate ecr" in line.strip("\n"):
+                        f.write(
+                            "gate rzx(param0) q0,q1 { h q1; cx q0,q1; rz(param0) q1; cx q0,q1; h q1; }\n"
+                        )
+                        f.write(
+                            "gate ecr q0,q1 { rzx(pi/4) q0,q1; x q0; rzx(-pi/4) q0,q1; }\n"
+                        )
+
+            qc = QuantumCircuit.from_qasm_file(new_name)
+            print("New qasm file for: ", new_name)
+
+        elif "oqc_tket" in f:
+            with open(f, "r") as f:
+                lines = f.readlines()
+            new_name = os.path.join("qasm_compiled_postprocessed", filename)
+            with open(new_name, "w") as f:
+                count = 0
+                for line in lines:
+                    f.write(line)
+                    count += 1
+                    if count == 2:
+                        f.write(
+                            "gate rzx(param0) q0,q1 { h q1; cx q0,q1; rz(param0) q1; cx q0,q1; h q1; }\n"
+                        )
+                        f.write(
+                            "gate ecr q0,q1 { rzx(pi/4) q0,q1; x q0; rzx(-pi/4) q0,q1; }\n"
+                        )
+            qc = QuantumCircuit.from_qasm_file(new_name)
+            print("New qasm file for: ", new_name)
