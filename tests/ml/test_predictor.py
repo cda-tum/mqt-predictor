86f3c5c962c16e946aecead3451f0c7c532f9292
Nils Quetschlich <Nils.Quetschlich@tum.de>
2022-12-07 17:12:38 +0100

    🎨 adjusted packaging structure

--- tests/ml/test_predictor.py
+++ tests/ml/test_predictor.py
@@ -4,36 +4,35 @@ from unittest.mock import patch
 import pytest
 from mqt.bench import benchmark_generator
 
-from mqt.predictor.ml import helper
-from mqt.predictor.ml.Predictor import Predictor
+from mqt.predictor import ml
 
 
 @patch("matplotlib.pyplot.show")
 def test_predict(mock_show):
-    path = helper.get_path_trained_model() / "trained_clf.joblib"
+    path = ml.helper.get_path_trained_model() / "trained_clf.joblib"
     assert path.is_file()
     filename = "test_qasm.qasm"
     qc = benchmark_generator.get_benchmark("dj", 1, 8)
     qc.qasm(filename=filename)
-    predictor = Predictor()
+    predictor = ml.Predictor()
     prediction = predictor.predict(filename)
-    assert prediction >= 0 and prediction < len(helper.get_index_to_comppath_LUT())
+    assert prediction >= 0 and prediction < len(ml.helper.get_index_to_comppath_LUT())
     prediction = predictor.predict(qc.qasm())
-    assert prediction >= 0 and prediction < len(helper.get_index_to_comppath_LUT())
+    assert prediction >= 0 and prediction < len(ml.helper.get_index_to_comppath_LUT())
     prediction = predictor.predict("fail test")
     assert not prediction
 
     predictor.clf = None
     prediction = predictor.predict(filename)
     Path(filename).unlink()
-    assert prediction >= 0 and prediction < len(helper.get_index_to_comppath_LUT())
+    assert prediction >= 0 and prediction < len(ml.helper.get_index_to_comppath_LUT())
 
 
 @pytest.mark.parametrize(
-    "comp_path", list(range(len(helper.get_index_to_comppath_LUT())))
+    "comp_path", list(range(len(ml.helper.get_index_to_comppath_LUT())))
 )
 def test_compilation_paths(comp_path):
-    predictor = Predictor()
+    predictor = ml.Predictor()
     qc_qasm = benchmark_generator.get_benchmark("dj", 1, 2).qasm()
     res, device = predictor.compile_predicted_compilation_path(qc_qasm, comp_path)
     assert res and device
@@ -50,7 +49,7 @@ def test_compile_all_circuits_for_qc():
     qc = benchmark_generator.get_benchmark("dj", 1, 2)
     tmp_filename = "test.qasm"
     qc.qasm(filename=tmp_filename)
-    predictor = Predictor()
+    predictor = ml.Predictor()
     assert predictor.compile_all_circuits_for_qc(
         filename=tmp_filename,
         source_path=".",
@@ -61,7 +60,7 @@ def test_compile_all_circuits_for_qc():
 
 @patch("matplotlib.pyplot.show")
 def test_train_random_forest_classifier(mock_pyplot):
-    predictor = Predictor()
+    predictor = ml.Predictor()
     assert predictor.clf is None
     predictor.train_random_forest_classifier(visualize_results=False)
     if Path("non_zero_indices.npy").exists():
@@ -72,7 +71,7 @@ def test_train_random_forest_classifier(mock_pyplot):
 
 def test_generate_compiled_circuits():
 
-    predictor = Predictor()
+    predictor = ml.Predictor()
     source_path = "."
     target_path = Path("test_compiled_circuits")
     if not target_path.exists():
