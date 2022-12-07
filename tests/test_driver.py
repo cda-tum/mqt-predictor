5111d8b99184d62512013d426e7ab00a755b7743
Nils Quetschlich <Nils.Quetschlich@tum.de>
2022-12-07 09:01:08 +0100

    ✨ adjusted used method

--- tests/test_driver.py
+++ tests/test_driver.py
@@ -1,4 +1,4 @@
-from mqt.bench import get_benchmark
+from mqt.bench import get_one_benchmark
 from pytket import Circuit
 from qiskit import QuantumCircuit
 
@@ -6,7 +6,7 @@ from mqt.predictor.driver import compile
 
 
 def test_compile():
-    qc = get_benchmark("ghz", 1, 5)
+    qc = get_one_benchmark("ghz", 1, 5)
     for model in ["ML"]:
         qc_compiled, device = compile(qc, model=model)
         assert isinstance(qc_compiled, (QuantumCircuit, Circuit))
