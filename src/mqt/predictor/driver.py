acc7b5b3c8ea8c8ca48467deec809af6427736e7
Nils Quetschlich <Nils.Quetschlich@tum.de>
2022-12-08 10:54:00 +0100

    ✨ added RL prediction

--- src/mqt/predictor/driver.py
+++ src/mqt/predictor/driver.py
@@ -1,8 +1,15 @@
-from mqt.predictor import ml
+from mqt.predictor import ml, rl
 
 
-def compile(qc, model="ML"):
+def compile(qc, model="ML", opt_objective="fidelity"):
     if model == "ML":
         predictor = ml.Predictor()
         prediction = predictor.predict(qc)
         return predictor.compile_predicted_compilation_path(qc, prediction)
+    elif model == "RL":
+        predictor = rl.Predictor()
+        qc_compiled, device = predictor.compile(qc, opt_objective=opt_objective)
+        return qc_compiled, device
+
+    else:
+        raise ValueError("Choose between 'ML' and 'RL' Model.")
