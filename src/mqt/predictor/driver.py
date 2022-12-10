c2b573ad4219d0238dba18ac9460591c03169d74
Nils Quetschlich <Nils.Quetschlich@tum.de>
2022-12-10 08:07:26 +0100

    🎨 removed unnecessary variable assignment and returned results immediately

--- src/mqt/predictor/driver.py
+++ src/mqt/predictor/driver.py
@@ -8,8 +8,7 @@ def compile(qc, model="ML", opt_objective="fidelity"):
         return predictor.compile_predicted_compilation_path(qc, prediction)
     elif model == "RL":
         predictor = rl.Predictor()
-        qc_compiled, device = predictor.compile(qc, opt_objective=opt_objective)
-        return qc_compiled, device
+        return predictor.compile(qc, opt_objective=opt_objective)
 
     else:
         raise ValueError("Choose between 'ML' and 'RL' Model.")
