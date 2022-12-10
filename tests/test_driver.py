6f6fd4ca382cf5f6f1ac2c0eb17e32008d576f6a
Nils Quetschlich <Nils.Quetschlich@tum.de>
2022-12-10 10:31:20 +0100

    ✅ added RL test

--- tests/test_driver.py
+++ tests/test_driver.py
@@ -7,7 +7,7 @@ from mqt.predictor.driver import compile
 
 def test_compile():
     qc = get_benchmark("ghz", 1, 5)
-    for model in ["ML"]:
+    for model in ["ML", "RL"]:
         qc_compiled, device = compile(qc, model=model)
         assert isinstance(qc_compiled, (QuantumCircuit, Circuit))
         assert isinstance(device, str)
