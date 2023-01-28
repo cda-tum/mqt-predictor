f3d4848c6b35b5a350767edbb146a88646023080
Nils Quetschlich <Nils.Quetschlich@tum.de>
2023-01-28 14:30:37 +0100

    ✨ implemented new reward functions

--- src/mqt/predictor/rl/Predictor.py
+++ src/mqt/predictor/rl/Predictor.py
@@ -16,7 +16,7 @@ from pytket.passes import (
 from pytket.placement import GraphPlacement
 from qiskit import QuantumCircuit, transpile
 from sb3_contrib import MaskablePPO
-from sb3_contrib.common.maskable.policies import MaskableActorCriticPolicy
+from sb3_contrib.common.maskable.policies import MaskableMultiInputActorCriticPolicy
 from sb3_contrib.common.maskable.utils import get_action_masks
 
 from mqt.predictor import reward, rl
@@ -37,27 +37,29 @@ class Predictor:
         )
         env = rl.PredictorEnv(opt_objective)
         obs = env.reset(qc)
+
+        used_compilation_passes = []
         while True:
             action_masks = get_action_masks(env)
             action, _states = model.predict(obs, action_masks=action_masks)
             action = int(action)
             action_item = env.action_set.get(action)
-            if action_item in rl.helper.get_actions_devices():
-                device = action_item["name"]
+            used_compilation_passes.append(action_item["name"])
             obs, reward_val, done, info = env.step(action)
             if done:
-                return env.state, device
+                return env.state, used_compilation_passes
 
-    def evaluate_sample_circuit_using(self, file):
-        print(file)
+    def evaluate_sample_circuit(self, file):
+        print("Evaluate file:", file)
 
-        reward_functions = ["parallelism", "fidelity", "critical_depth"]
+        reward_functions = ["fidelity", "critical_depth", "gates", "mix"]
         for rew in reward_functions:
             model = MaskablePPO.load(
                 rl.helper.get_path_trained_model() / ("model_" + rew)
             )
             env = rl.PredictorEnv(rew)
             obs = env.reset(file)
+
             qc = env.state
             start_time = time.time()
             while True:
@@ -65,33 +67,30 @@ class Predictor:
                 action, _states = model.predict(obs, action_masks=action_masks)
                 action = int(action)
                 obs, reward_val, done, info = env.step(action)
+
                 if done:
                     duration = time.time() - start_time
                     if rew == "fidelity":
-                        RL_fid = np.round(
-                            reward.expected_fidelity(env.state, env.device), 2
-                        )
+                        RL_fid = reward.expected_fidelity(env.state, env.device)
                         RL_fid_time = np.round(duration, 2)
-                        RL_fid_crit_depth = np.round(reward.crit_depth(env.state), 2)
-                        RL_fid_parallelism = np.round(reward.parallelism(env.state), 2)
-                    elif rew == "parallelism":
-                        RL_parallelism = np.round(reward.parallelism(env.state), 2)
-                        RL_parallelism_time = np.round(duration, 2)
-                        RL_parallelism_fid = np.round(
-                            reward.expected_fidelity(env.state, env.device), 2
+                        RL_fid_crit_depth = reward.crit_depth(env.state)
+                        RL_fid_gate_ratio = reward.gate_ratio(env.state)
+                    elif rew == "gates":
+                        RL_gate_ratio = reward.gate_ratio(env.state)
+                        RL_gate_ratio_time = np.round(duration, 2)
+                        RL_gate_ratio_fid = reward.expected_fidelity(
+                            env.state, env.device
                         )
-                        RL_parallelism_crit_depth = np.round(
-                            reward.crit_depth(env.state), 2
-                        )
+                        RL_gate_ratio_crit_depth = reward.crit_depth(env.state)
                     elif rew == "critical_depth":
-                        RL_crit_depth = np.round(reward.crit_depth(env.state), 2)
+                        RL_crit_depth = reward.crit_depth(env.state)
                         RL_crit_depth_time = np.round(duration, 2)
-                        RL_crit_depth_fid = np.round(
-                            reward.expected_fidelity(env.state, env.device), 2
+                        RL_crit_depth_fid = reward.expected_fidelity(
+                            env.state, env.device
                         )
-                        RL_crit_depth_parallelism = np.round(
-                            reward.parallelism(env.state), 2
-                        )
+                        RL_crit_depth_gate_ratio = reward.gate_ratio(env.state)
+                    elif rew == "mix":
+                        RL_mix = reward.mix(env.state, env.device)
                     break
 
         start_time = time.time()
@@ -103,11 +102,10 @@ class Predictor:
             seed_transpiler=1,
         )
         duration = time.time() - start_time
-        qiskit_o3_fid = np.round(
-            reward.expected_fidelity(transpiled_qc_qiskit, "ibm_washington"), 2
-        )
+        qiskit_o3_fid = reward.expected_fidelity(transpiled_qc_qiskit, "ibm_washington")
         qiskit_o3_crit_depth = reward.crit_depth(transpiled_qc_qiskit)
-        qiskit_o3_parallel = reward.parallelism(transpiled_qc_qiskit)
+        qiskit_o3_gate_ratio = reward.gate_ratio(transpiled_qc_qiskit)
+        qiskit_o3_mix = reward.mix(transpiled_qc_qiskit, "ibm_washington")
         qiskit_o3_time = np.round(duration, 2)
 
         tket_qc = qiskit_to_tk(qc)
@@ -128,39 +126,42 @@ class Predictor:
 
         transpiled_qc_tket = tk_to_qiskit(tket_qc)
 
-        tket_fid = np.round(
-            reward.expected_fidelity(transpiled_qc_tket, "ibm_washington"), 2
-        )
+        tket_fid = reward.expected_fidelity(transpiled_qc_tket, "ibm_washington")
         tket_crit_depth = reward.crit_depth(transpiled_qc_tket)
-        tket_parallelism = reward.parallelism(transpiled_qc_tket)
+        tket_gate_ratio = reward.gate_ratio(transpiled_qc_tket)
+        tket_mix = reward.mix(transpiled_qc_tket, "ibm_washington")
         tket_time = np.round(duration, 2)
 
-        return (
+        res = (
             str(file).split("/")[-1].split(".")[0].replace("_", " ").split(" ")[0],
             str(file).split("/")[-1].split(".")[0].replace("_", " ").split(" ")[-1],
             qiskit_o3_fid,
             tket_fid,
             RL_fid,
-            qiskit_o3_parallel,
-            tket_parallelism,
-            RL_parallelism,
+            qiskit_o3_gate_ratio,
+            tket_gate_ratio,
+            RL_gate_ratio,
             qiskit_o3_crit_depth,
             tket_crit_depth,
             RL_crit_depth,
+            qiskit_o3_mix,
+            tket_mix,
+            RL_mix,
             qiskit_o3_time,
             tket_time,
             RL_fid_time,
-            RL_parallelism_time,
+            RL_gate_ratio_time,
             RL_crit_depth_time,
             RL_fid_crit_depth,
-            RL_fid_parallelism,
+            RL_fid_gate_ratio,
             RL_crit_depth_fid,
-            RL_crit_depth_parallelism,
-            RL_parallelism_fid,
-            RL_parallelism_crit_depth,
+            RL_crit_depth_gate_ratio,
+            RL_gate_ratio_fid,
+            RL_gate_ratio_crit_depth,
         )
+        return res
 
-    def eval_all_sample_circuits(self):
+    def evaluate_all_sample_circuits(self):
         res_csv = []
         res_csv.append(
             (
@@ -169,23 +170,26 @@ class Predictor:
                 "Qiskit O3 Fid",
                 "TKET Fid",
                 "RL Fid",
-                "Qiskit O3 Parallelism",
-                "TKET Parallelism",
-                "RL Parallelism",
+                "Qiskit O3 gate_ratio",
+                "TKET gate_ratio",
+                "RL gate_ratio",
                 "Qiskit O3 Crit Depth",
                 "TKET Crit Depth",
                 "RL Crit Depth",
+                "Qiskit O3 mix",
+                "TKET mix",
+                "RL mix",
                 "Qiskit O3 Time",
                 "TKET Time",
                 "RL Fid Time",
-                "RL Parallelism Time",
+                "RL gate_ratio Time",
                 "RL Crit Depth Time",
                 "RL Fid Crit Depth",
-                "RL Fid Parallelism",
+                "RL Fid gate_ratio",
                 "RL Crit Depth Fid",
-                "RL Crit Depth Parallelism",
-                "RL Parallelism Fid",
-                "RL Parallelism Crit Depth",
+                "RL Crit Depth gate_ratio",
+                "RL gate_ratio Fid",
+                "RL gate_ratio Crit Depth",
             )
         )
 
@@ -195,7 +199,6 @@ class Predictor:
         )
         for res in results:
             res_csv.append(res)
-        # print(results)
         np.savetxt(
             rl.helper.get_path_trained_model() / "res.csv",
             res_csv,
@@ -203,23 +206,14 @@ class Predictor:
             fmt="%s",
         )
 
-    def instantiate_models(self, timesteps, fid, dep, par, model_name="training"):
-        self.train_all_models(
-            timestep=timesteps,
-            verbose=2,
-            fid=fid,
-            dep=dep,
-            par=par,
-            model_name=model_name,
-        )
-
     def train_all_models(
         self,
-        timestep=1000,
+        timesteps=1000,
         verbose=2,
         fid=False,
         dep=False,
-        par=False,
+        mix=False,
+        gates=False,
         model_name="model",
     ):
         reward_functions = []  # ["fidelity", "depth", "two_qubit_count"]
@@ -227,19 +221,29 @@ class Predictor:
             reward_functions.append("fidelity")
         if dep:
             reward_functions.append("critical_depth")
-        if par:
-            reward_functions.append("parallelism")
+        if mix:
+            reward_functions.append("mix")
+        if gates:
+            reward_functions.append("gates")
+
+        if "test" in model_name:
+            n_steps = 100
+            progress_bar = False
+        else:
+            n_steps = 2048
+            progress_bar = True
 
         for rew in reward_functions:
             print("Start training for: ", rew)
-            env = rl.PhaseOrdererEnv(reward_function=rew)
+            env = rl.PredictorEnv(reward_function=rew)
 
             model = MaskablePPO(
-                MaskableActorCriticPolicy,
+                MaskableMultiInputActorCriticPolicy,
                 env,
                 verbose=verbose,
                 tensorboard_log="./" + model_name + "_" + rew,
-                gamma=0.98,
+                gamma=0.95,
+                n_steps=n_steps,
             )
-            model.learn(total_timesteps=timestep, progress_bar=False)
+            model.learn(total_timesteps=timesteps, progress_bar=progress_bar)
             model.save(rl.helper.get_path_trained_model() / (model_name + "_" + rew))
