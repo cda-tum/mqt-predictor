a8234fbba26fac08b0c219797dbe3ce4d5889497
Nils Quetschlich <Nils.Quetschlich@tum.de>
2023-01-30 08:45:53 +0100

    🚨 isort

--- src/mqt/predictor/rl/helper.py
+++ src/mqt/predictor/rl/helper.py
@@ -5,7 +5,6 @@ from pathlib import Path
 
 import numpy as np
 import requests
-from packaging import version
 from pytket import architecture
 from pytket.circuit import OpType
 from pytket.passes import (
@@ -45,6 +44,7 @@ from sb3_contrib import MaskablePPO
 from tqdm import tqdm
 
 from mqt.predictor import rl, utils
+from packaging import version
 
 if sys.version_info < (3, 10, 0):
     import importlib_metadata as metadata
