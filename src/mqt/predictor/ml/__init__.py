c5406c5f7023f5dbae86afc6c00c74cf7b8b6cff
Nils Quetschlich <Nils.Quetschlich@tum.de>
2023-08-16 16:58:55 +0200

    🚚 adjusted method name

--- src/mqt/predictor/ml/__init__.py
+++ src/mqt/predictor/ml/__init__.py
@@ -1,7 +1,7 @@
-from mqt.predictor.ml.helper import predict
+from mqt.predictor.ml.helper import qcompile
 from mqt.predictor.ml.Predictor import Predictor
 
 __all__ = [
-    "predict",
+    "qcompile",
     "Predictor",
 ]
