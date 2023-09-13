9a1af22e2a13de601a2b14efed17a011f6aa23a7
Nils Quetschlich <Nils.Quetschlich@tum.de>
2023-09-13 16:12:10 +0200

    ✅ adjusted test for CI

--- tests/compilation/test_predictor_rl.py
+++ tests/compilation/test_predictor_rl.py
@@ -1,19 +1,28 @@
+import os
+
 import pytest
 from mqt.bench import get_benchmark
 from mqt.predictor import reward, rl
 from qiskit import QuantumCircuit
 
-# @pytest.mark.parametrize(
-#     "figure_of_merit",
-#     ["fidelity", "critical_depth"],
-# )
-# def test_qcompile_with_pretrained_models(figure_of_merit: reward.reward_functions) -> None:
-#     qc = get_benchmark("ghz", 1, 5)
-#     res = rl.qcompile(qc, figure_of_merit=figure_of_merit, device_name="ibm_washington")
-#     assert type(res) == tuple
-#     qc_compiled, compilation_information = res
-#     assert isinstance(qc_compiled, QuantumCircuit)
-#     assert compilation_information is not None
+IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
+
+
+@pytest.mark.skipif(
+    IN_GITHUB_ACTIONS,
+    reason="Trained models are not yet available. Skipping may be reverted after release with trained models.",
+)
+@pytest.mark.parametrize(
+    "figure_of_merit",
+    ["fidelity", "critical_depth"],
+)
+def test_qcompile_with_pretrained_models(figure_of_merit: reward.reward_functions) -> None:
+    qc = get_benchmark("ghz", 1, 5)
+    res = rl.qcompile(qc, figure_of_merit=figure_of_merit, device_name="ibm_washington")
+    assert type(res) == tuple
+    qc_compiled, compilation_information = res
+    assert isinstance(qc_compiled, QuantumCircuit)
+    assert compilation_information is not None
 
 
 def test_instantiate_models() -> None:
