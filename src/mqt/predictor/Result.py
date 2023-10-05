4232f9e0d20c55abf6729a505b285f07b6c3b8a7
Nils Quetschlich <Nils.Quetschlich@tum.de>
2023-10-05 08:28:34 +0200

    🎨 improved the Result class

--- src/mqt/predictor/Result.py
+++ src/mqt/predictor/Result.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from mqt.predictor import reward, rl
+from mqt.predictor import reward
 
 if TYPE_CHECKING:
     from qiskit import QuantumCircuit
@@ -14,9 +14,9 @@ class Result:
 
     Attributes:
         benchmark (str): The path to the benchmark to be compiled.
-        used_setup (str): The setup used for compilation. Either 'mqt-predictor', 'qiskit_o3' or 'tket'.
-        duration (float): The time it took to compile the benchmark.
-        qc (QuantumCircuit | None): The compiled quantum circuit. If compilation failed, None is returned.
+        compiler (str): The setup used for compilation. Either 'mqt-predictor', 'qiskit_o3' or 'tket'.
+        compilation_time (float): The time it took to compile the benchmark.
+        compiled_qc (QuantumCircuit | None): The compiled quantum circuit. If compilation failed, None is returned.
         device (str): The device used for compilation.
 
     """
@@ -24,57 +24,29 @@ class Result:
     def __init__(
         self,
         benchmark: str,
-        used_setup: str,
-        duration: float,
-        qc: QuantumCircuit | None,
+        compiler: str,
+        compilation_time: float,
+        compiled_qc: QuantumCircuit | None,
         device: str,
     ) -> None:
-        if qc is not None:
-            rew_fid = reward.expected_fidelity(qc, device)
-            rew_crit_depth = reward.crit_depth(qc)
+        if compiled_qc is not None:
+            rew_fid = reward.expected_fidelity(compiled_qc, device)
+            rew_crit_depth = reward.crit_depth(compiled_qc)
         else:
-            rew_fid = -1
-            rew_crit_depth = -1
+            rew_fid = -1.0
+            rew_crit_depth = -1.0
 
-        self.benchmark: str = benchmark
-        self.used_setup: str = used_setup
-        self.time: float = duration
-        self.fidelity: float = rew_fid
-        self.critical_depth: float = rew_crit_depth
+        self.benchmark = benchmark
+        self.compiler = compiler
+        self.compilation_time = compilation_time
+        self.fidelity = rew_fid
+        self.critical_depth = rew_crit_depth
 
     def get_dict(self) -> dict[str, float]:
         """Returns the results as a dictionary."""
 
         return {
-            self.used_setup + "_" + "time": self.time,
-            self.used_setup + "_" + "fidelity": self.fidelity,
-            self.used_setup + "_" + "critical_depth": self.critical_depth,
+            self.compiler + "_" + "time": self.compilation_time,
+            self.compiler + "_" + "fidelity": self.fidelity,
+            self.compiler + "_" + "critical_depth": self.critical_depth,
         }
-
-
-class MQTDurationResult:
-    """
-    The MQTDurationResult class is used to store the results of a compilation.
-    """
-
-    def __init__(
-        self,
-        benchmark: str,
-        figure_of_merit: str,
-        res_mqt: list[float],
-    ) -> None:
-        self.benchmark: str = benchmark
-        self.res_mqt = res_mqt
-        self.figure_of_merit = figure_of_merit
-
-    def get_dict(self) -> dict[str, float | str]:
-        """Returns the results as a dictionary."""
-        overall: dict[str, float | str] = {"benchmark": self.benchmark}
-        for i, dev in enumerate(rl.helper.get_devices()):
-            overall.update(
-                {
-                    "mqt_duration_" + self.figure_of_merit + "_" + dev["name"]: self.res_mqt[i],
-                }
-            )
-
-        return overall
--- tests/test_result.py
+++ tests/test_result.py
@@ -7,5 +7,7 @@ def test_result() -> None:
     qc = get_benchmark("ghz", 1, 5)
     for device in rl.helper.get_devices():
         qc_compiled = transpile(qc, basis_gates=device["native_gates"], coupling_map=device["cmap"])
-        res = Result("test_circuit", used_setup="test", duration=1.0, qc=qc_compiled, device=device["name"])
+        res = Result(
+            "test_circuit", compiler="test", compilation_time=1.0, compiled_qc=qc_compiled, device=device["name"]
+        )
         assert res is not None
