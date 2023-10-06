f6d83a150a1459430a3d9a3c195f8adfe3a4f6f1
Nils Quetschlich <Nils.Quetschlich@tum.de>
2023-10-06 12:17:23 +0200

    ✅ improved tests

--- tests/device_selection/test_predictor_ml.py
+++ tests/device_selection/test_predictor_ml.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Final
 
 import pytest
 from mqt.bench import benchmark_generator
@@ -9,7 +10,7 @@ def test_predict() -> None:
     path = ml.helper.get_path_trained_model(figure_of_merit="expected_fidelity")
     assert path.is_file()
     filename = "test_qasm.qasm"
-    figure_of_merit: reward.figure_of_merit = "expected_fidelity"
+    figure_of_merit: Final = "expected_fidelity"
     qc = benchmark_generator.get_benchmark("dj", 1, 8)
     qc.qasm(filename=filename)
     predictor = ml.Predictor()
