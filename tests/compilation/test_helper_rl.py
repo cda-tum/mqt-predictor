6dc590f8d95157370cdf8eff09621028e0f5e03b
Nils Quetschlich <Nils.Quetschlich@tum.de>
2023-10-06 08:56:10 +0200

    ✅ refactored tests

--- tests/compilation/test_helper_rl.py
+++ tests/compilation/test_helper_rl.py
@@ -2,10 +2,7 @@ from pathlib import Path
 
 import pytest
 from mqt.bench import get_benchmark
-from mqt.bench.qiskit_helper import get_native_gates
-from mqt.bench.utils import get_cmap_from_devicename
 from mqt.predictor import rl
-from qiskit import QuantumCircuit
 
 
 def test_get_actions_opt() -> None:
@@ -36,55 +33,6 @@ def test_get_actions_mapping() -> None:
     assert len(rl.helper.get_actions_mapping()) == rl.helper.NUM_ACTIONS_MAPPING
 
 
-def test_get_random_state_sample() -> None:
-    sample, _ = rl.helper.get_state_sample()
-    assert sample
-    assert isinstance(sample, QuantumCircuit)
-
-
-@pytest.mark.parametrize(
-    ("device", "gate_set"),
-    [
-        ("ibm", ["rz", "sx", "x", "cx", "measure"]),
-        ("ionq", ["rxx", "rz", "ry", "rx", "measure"]),
-        ("rigetti", ["rx", "rz", "cz", "measure"]),
-        ("oqc", ["rz", "sx", "x", "ecr", "measure"]),
-        ("quantinuum", ["rzz", "rz", "ry", "rx", "measure"]),
-    ],
-)
-def test_get_native_gatesets(device: str, gate_set: list[str]) -> None:
-    assert get_native_gates(device) == gate_set
-
-
-NUM_CONNECTIONS_RIGETTI_M2 = 212
-
-
-def test_get_rigetti_aspen_m2_map() -> None:
-    assert len(get_cmap_from_devicename("rigetti_aspen_m2")) == NUM_CONNECTIONS_RIGETTI_M2
-
-
-NUM_CONNECTIONS_IONQ_HARMONY = 110
-
-
-def test_get_ionq_harmony_c_map() -> None:
-    assert len(get_cmap_from_devicename("ionq_harmony")) == NUM_CONNECTIONS_IONQ_HARMONY
-
-
-NUM_CONNECTIONS_OQC_LUCY = 8
-
-
-def test_get_cmap_oqc_lucy() -> None:
-    assert len(get_cmap_from_devicename("oqc_lucy")) == NUM_CONNECTIONS_OQC_LUCY
-
-
-@pytest.mark.parametrize(
-    "device",
-    ["ibm_washington", "ibm_montreal", "rigetti_aspen_m2", "oqc_lucy", "ionq_harmony", "ionq_aria1", "quantinuum_h2"],
-)
-def test_get_cmap_from_devicename(device: str) -> None:
-    assert get_cmap_from_devicename(device)
-
-
 @pytest.mark.parametrize(
     "device",
     ["ibm_washington", "ibm_montreal", "rigetti_aspen_m2", "oqc_lucy", "ionq_harmony", "ionq_aria1", "quantinuum_h2"],
@@ -93,22 +41,11 @@ def test_get_device(device: str) -> None:
     assert rl.helper.get_device(device)
 
 
-@pytest.mark.parametrize(
-    "platform",
-    ["ibm", "rigetti", "oqc", "ionq", "quantinuum"],
-)
-def test_get_native_gates_from_platform_name(platform: str) -> None:
-    assert get_native_gates(platform)
-
-
-NUM_FEATURES = 7
-
-
 def test_create_feature_dict() -> None:
     qc = get_benchmark("dj", 1, 5)
     features = rl.helper.create_feature_dict(qc)
     assert features
-    assert len(features) == NUM_FEATURES
+    assert len(features) == rl.helper.NUM_FEATURE_VECTOR_ELEMENTS
 
 
 def test_get_path_trained_model() -> None:
