c383a8a97431fc60896aa939eef44c5ff0cd53cc
Nils Quetschlich <Nils.Quetschlich@tum.de>
2024-02-23 15:50:04 +0100

    🎨 remove newly created model after usage

--- tests/compilation/test_predictor_rl.py
+++ tests/compilation/test_predictor_rl.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import os
+from pathlib import Path
 
 import pytest
 from qiskit import QuantumCircuit
@@ -16,19 +17,24 @@ IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
     ["expected_fidelity", "critical_depth"],
 )
 def test_qcompile_with_newly_trained_models(figure_of_merit: reward.figure_of_merit) -> None:
-    predictor = rl.Predictor(figure_of_merit=figure_of_merit, device_name="ionq_harmony")
+    device = "ionq_harmony"
+    predictor = rl.Predictor(figure_of_merit=figure_of_merit, device_name=device)
     predictor.train_model(
         timesteps=100,
         test=True,
     )
 
     qc = get_benchmark("ghz", 1, 5)
-    res = rl.qcompile(qc, figure_of_merit=figure_of_merit, device_name="ionq_harmony")
+    res = rl.qcompile(qc, figure_of_merit=figure_of_merit, device_name=device)
     assert type(res) == tuple
     qc_compiled, compilation_information = res
 
     assert isinstance(qc_compiled, QuantumCircuit)
     assert compilation_information is not None
+
+    model_path = rl.helper.get_path_trained_model()
+    if model_path.exists():
+        Path(model_path / ("model_" + figure_of_merit + "_" + device + ".zip")).unlink()
 
 
 def test_qcompile_with_false_input() -> None:
