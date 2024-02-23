2628dab7896499498fe080441dc35418e4486fda
Nils Quetschlich <nils.quetschlich@tum.de>
2024-02-23 19:22:54 +0100

    Bugfix testing (#203)
    Removed unnecessary statement and added test for pretrained model
    testing which is triggered manually via GitHub CI.
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/compilation/test_pretrained_models.py
+++ tests/compilation/test_pretrained_models.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
 
 import os
+from pathlib import Path
 
 import pytest
 from qiskit import QuantumCircuit
 
 from mqt.bench import get_benchmark
-from mqt.predictor import reward, rl
+from mqt.bench.devices import get_available_device_names
+from mqt.predictor import qcompile, reward, rl
+from mqt.predictor.evaluation import evaluate_sample_circuit
 
 # only run test when executed on GitHub runner
 IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
@@ -22,3 +25,31 @@ def test_qcompile_with_pretrained_models(figure_of_merit: reward.figure_of_merit
     qc_compiled, compilation_information = rl.qcompile(qc, figure_of_merit=figure_of_merit, device_name="quantinuum_h2")
     assert isinstance(qc_compiled, QuantumCircuit)
     assert compilation_information is not None
+
+
+def test_qcompile() -> None:
+    qc = get_benchmark("ghz", 1, 5)
+    qc_compiled, compilation_information, quantum_device = qcompile(qc)
+    assert quantum_device in get_available_device_names()
+    assert qc_compiled.layout is not None
+    assert len(qc_compiled) > 0
+
+
+def test_evaluate_sample_circuit() -> None:
+    qc = get_benchmark("ghz", 1, 3)
+    filename = "test_3.qasm"
+    qc.qasm(filename=filename)
+    res = evaluate_sample_circuit(filename)
+    expected_keys = []
+    for compilation_setup in ["qiskit", "tket", "mqt-predictor_expected_fidelity", "mqt-predictor_critical_depth"]:
+        for key in ["time", "expected_fidelity", "critical_depth"]:
+            if "mqt-predictor" in compilation_setup:
+                expected_keys.append(compilation_setup + "_" + key)
+            else:
+                expected_keys.extend(
+                    [compilation_setup + "_" + device["name"] + "_" + key for device in rl.helper.get_devices()]
+                )
+
+    assert all(key in res for key in expected_keys)
+    if Path(filename).exists():
+        Path(filename).unlink()
