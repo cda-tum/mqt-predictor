7f39dbfb263c4c300c79e8bba36f677782c8dbd4
Nils Quetschlich <Nils.Quetschlich@tum.de>
2024-02-26 09:31:45 +0100

    ✅ removed unnecessary test

--- tests/compilation/test_predictor_rl.py
+++ tests/compilation/test_predictor_rl.py
@@ -11,23 +11,12 @@ from mqt.predictor import reward, rl
     "figure_of_merit",
     ["expected_fidelity", "critical_depth"],
 )
-def test_qcompile_with_pretrained_models(figure_of_merit: reward.figure_of_merit) -> None:
-    qc = get_benchmark("ghz", 1, 3)
-    qc_compiled, compilation_information = rl.qcompile(qc, figure_of_merit=figure_of_merit, device_name="ionq_harmony")
-    assert isinstance(qc_compiled, QuantumCircuit)
-    assert compilation_information is not None
-
-
-@pytest.mark.parametrize(
-    "figure_of_merit",
-    ["expected_fidelity", "critical_depth"],
-)
 def test_qcompile_with_newly_trained_models(figure_of_merit: reward.figure_of_merit) -> None:
-    predictor = rl.Predictor(figure_of_merit=figure_of_merit, device_name="ionq_harmony")
+    predictor = rl.Predictor(figure_of_merit=figure_of_merit, device_name="ibm_guadalupe")
     predictor.train_model(timesteps=200, test=True)
 
     qc = get_benchmark("ghz", 1, 5)
-    res = rl.qcompile(qc, figure_of_merit=figure_of_merit, device_name="ionq_harmony")
+    res = rl.qcompile(qc, figure_of_merit=figure_of_merit, device_name="ibm_guadalupe")
     assert type(res) == tuple
     qc_compiled, compilation_information = res
 
