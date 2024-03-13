5d0d1811d833192328e0aea82d3d391b24b89610
Nils Quetschlich <Nils.Quetschlich@tum.de>
2024-03-13 18:46:07 +0100

    ♻️ refactored evaluation

--- evaluation_helper.py
+++ evaluation_helper.py
@@ -1,9 +1,13 @@
+from __future__ import annotations
+
 import pathlib
+
 import numpy as np
 from matplotlib import pyplot as plt
 
 plt.rcParams["font.family"] = "Times New Roman"
 plt.rcParams["font.size"] = 16
+
 
 def read_mqt_predictor_file(num_qubits: int, device: str) -> list[float]:
     best_res = [1.0]
@@ -27,20 +31,25 @@ def read_baseline_date(path: str) -> tuple[np.ndarray, np.ndarray, np.ndarray, n
     best_run = sorted_data[0]
     worst_run = sorted_data[-1]
     median_run = sorted_data[len(sorted_data) // 2]
-    worst_overall_values = np.maximum.reduce([baseline for baseline in sorted_data])
-    best_overall_values = np.minimum.reduce([baseline for baseline in sorted_data])
-    return best_run, worst_run, median_run, best_overall_values, worst_overall_values,
+    worst_overall_values = np.maximum.reduce(list(sorted_data))
+    best_overall_values = np.minimum.reduce(list(sorted_data))
+    return (
+        best_run,
+        worst_run,
+        median_run,
+        best_overall_values,
+        worst_overall_values,
+    )
 
 
 def generate_eval_plot(
-        best_overall_O3: np.ndarray,
-        best_overall_O1: np.ndarray,
-        worst_overall_O3: np.ndarray,
-        worst_overall_O1: np.ndarray,
-        mqt_predictor: list[float],
-        device: str,
-        num_qubits: int,
-
+    best_overall_O3: np.ndarray,
+    best_overall_O1: np.ndarray,
+    worst_overall_O3: np.ndarray,
+    worst_overall_O1: np.ndarray,
+    mqt_predictor: list[float],
+    device: str,
+    num_qubits: int,
 ):
     for run in [best_overall_O3, best_overall_O1, worst_overall_O3, worst_overall_O1, mqt_predictor]:
         local_min = 1
@@ -53,9 +62,22 @@ def generate_eval_plot(
     linewidth = 2.5
     plt.plot(mqt_predictor, label="Proposed Approach", color=colors[4], linewidth=linewidth)
 
-    plt.fill_between(range(len(best_overall_O1)), best_overall_O1, worst_overall_O1, alpha=0.2, color=colors[0], label="Qiskit Default")
-    plt.fill_between(range(len(best_overall_O3)), best_overall_O3, worst_overall_O3, alpha=0.5, color=colors[2], label="Qiskit Most-optimized")
-
+    plt.fill_between(
+        range(len(best_overall_O1)),
+        best_overall_O1,
+        worst_overall_O1,
+        alpha=0.2,
+        color=colors[0],
+        label="Qiskit Default",
+    )
+    plt.fill_between(
+        range(len(best_overall_O3)),
+        best_overall_O3,
+        worst_overall_O3,
+        alpha=0.5,
+        color=colors[2],
+        label="Qiskit Most-optimized",
+    )
 
     plt.title(f"Evaluation for {num_qubits} qubits on {device}")
     plt.ylabel("KL Divergence")
