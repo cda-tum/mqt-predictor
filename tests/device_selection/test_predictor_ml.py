b64e13653bd3de3d95d90c80c6b4b4e713701b76
Nils Quetschlich <Nils.Quetschlich@tum.de>
2024-07-15 15:33:26 -0700

    🐛 debugging trial and error

--- tests/device_selection/test_predictor_ml.py
+++ tests/device_selection/test_predictor_ml.py
@@ -2,12 +2,13 @@
 
 from __future__ import annotations
 
+import sys
 from pathlib import Path
 from typing import Literal
 
 import numpy as np
 import pytest
-from qiskit.qasm2 import dump
+from qiskit.qasm2 import dump, dumps
 
 from mqt.bench import benchmark_generator
 from mqt.bench.devices import get_available_device_names, get_available_devices
