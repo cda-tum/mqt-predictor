2ce7405ce2127801020cc8c6b20ded72b7d3a764
flowerthrower <flowerthrower@users.noreply.github.com>
2024-09-27 15:04:00 +0200

    enable tests, fix compatible versions

--- tests/test_pretrained_models.py
+++ tests/test_pretrained_models.py
@@ -16,7 +16,7 @@ from mqt.predictor.evaluation import evaluate_sample_circuit
 IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
 
 
-@pytest.mark.skipif(not IN_GITHUB_ACTIONS, reason="Only run this test on GitHub runner")
+#@pytest.mark.skipif(not IN_GITHUB_ACTIONS, reason="Only run this test on GitHub runner")
 @pytest.mark.parametrize(
     "figure_of_merit",
     ["expected_fidelity", "critical_depth"],
@@ -28,7 +28,7 @@ def test_qcompile_with_pretrained_models(figure_of_merit: reward.figure_of_merit
     assert compilation_information is not None
 
 
-@pytest.mark.skipif(not IN_GITHUB_ACTIONS, reason="Only run this test on GitHub runner")
+#@pytest.mark.skipif(not IN_GITHUB_ACTIONS, reason="Only run this test on GitHub runner")
 def test_qcompile() -> None:
     qc = get_benchmark("ghz", 1, 5)
     qc_compiled, _compilation_information, quantum_device = qcompile(qc)
