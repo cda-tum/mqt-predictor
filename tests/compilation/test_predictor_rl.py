9fcc166163f12ddb7a4e075dbbc1782d93668192
Nils Quetschlich <nils.quetschlich@tum.de>
2024-11-15 15:10:29 +0100

    🐛 trial and error

--- tests/compilation/test_predictor_rl.py
+++ tests/compilation/test_predictor_rl.py
@@ -1,34 +1,34 @@
 """Tests for the compilation with reinforcement learning."""
-#
-# from __future__ import annotations
-#
-# from pathlib import Path
-# from typing import get_args
-#
-# import pytest
-# from qiskit import QuantumCircuit
-# from qiskit.qasm2 import dump
-#
-# from mqt.bench import get_benchmark
-# from mqt.predictor import reward, rl
-#
-#
-# def test_predictor_env_reset_from_string() -> None:
-#     """Test the reset function of the predictor environment with a quantum circuit given as a string as input."""
-#     predictor = rl.Predictor(figure_of_merit="expected_fidelity", device_name="ionq_harmony")
-#     qasm_path = Path("test.qasm")
-#     qc = get_benchmark("dj", 1, 3)
-#     with qasm_path.open("w", encoding="utf-8") as f:
-#         dump(qc, f)
-#     assert predictor.env.reset(qc=qasm_path)[0] == rl.helper.create_feature_dict(qc)
-#
-#
-# def test_predictor_env_esp_error() -> None:
-#     """Test the predictor environment with ESP as figure of merit and missing calibration data."""
-#     with pytest.raises(ValueError, match="Missing calibration data for ESP calculation on ibm_montreal."):
-#         rl.Predictor(figure_of_merit="estimated_success_probability", device_name="ibm_montreal")
-#
-#
+
+from __future__ import annotations
+
+from pathlib import Path
+from typing import get_args
+
+import pytest
+from qiskit import QuantumCircuit
+from qiskit.qasm2 import dump
+
+from mqt.bench import get_benchmark
+from mqt.predictor import reward, rl
+
+
+def test_predictor_env_reset_from_string() -> None:
+    """Test the reset function of the predictor environment with a quantum circuit given as a string as input."""
+    predictor = rl.Predictor(figure_of_merit="expected_fidelity", device_name="ionq_harmony")
+    qasm_path = Path("test.qasm")
+    qc = get_benchmark("dj", 1, 3)
+    with qasm_path.open("w", encoding="utf-8") as f:
+        dump(qc, f)
+    assert predictor.env.reset(qc=qasm_path)[0] == rl.helper.create_feature_dict(qc)
+
+
+def test_predictor_env_esp_error() -> None:
+    """Test the predictor environment with ESP as figure of merit and missing calibration data."""
+    with pytest.raises(ValueError, match="Missing calibration data for ESP calculation on ibm_montreal."):
+        rl.Predictor(figure_of_merit="estimated_success_probability", device_name="ibm_montreal")
+
+
 # @pytest.mark.parametrize(
 #     "figure_of_merit",
 #     get_args(reward.figure_of_merit),
@@ -62,12 +62,12 @@
 #     assert isinstance(qc_compiled, QuantumCircuit)
 #     assert qc_compiled.layout is not None
 #     assert compilation_information is not None
-#
-#
-# def test_qcompile_with_false_input() -> None:
-#     """Test the qcompile function with false input."""
-#     qc = get_benchmark("dj", 1, 5)
-#     with pytest.raises(ValueError, match="figure_of_merit must not be None if predictor_singleton is None."):
-#         rl.helper.qcompile(qc, None, "quantinuum_h2")
-#     with pytest.raises(ValueError, match="device_name must not be None if predictor_singleton is None."):
-#         rl.helper.qcompile(qc, "expected_fidelity", None)
+
+
+def test_qcompile_with_false_input() -> None:
+    """Test the qcompile function with false input."""
+    qc = get_benchmark("dj", 1, 5)
+    with pytest.raises(ValueError, match="figure_of_merit must not be None if predictor_singleton is None."):
+        rl.helper.qcompile(qc, None, "quantinuum_h2")
+    with pytest.raises(ValueError, match="device_name must not be None if predictor_singleton is None."):
+        rl.helper.qcompile(qc, "expected_fidelity", None)
