828b3c04b3c92bf5bf095914f922d51f26dc9ee8
Nils Quetschlich <nils.quetschlich@tum.de>
2024-11-20 15:13:43 +0100

    Expected success probability Docu and Tests (#321)
    This PR re-adds tests that have been temporarily deactivated to
    accelerate debugging of the original PR in #212.
    
    ---------
    
    Signed-off-by: burgholzer <burgholzer@me.com>
    Signed-off-by: Patrick Hopf <81010725+flowerthrower@users.noreply.github.com>
    Signed-off-by: Nils Quetschlich <nils.quetschlich@tum.de>
    Co-authored-by: Patrick Hopf <>
    Co-authored-by: Patrick Hopf <81010725+flowerthrower@users.noreply.github.com>
    Co-authored-by: Patrick Hopf <asdf>
    Co-authored-by: Patrick Hopf <patrick.hopf@tum.de>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: flowerthrower <flowerthrower@users.noreply.github.com>
    Co-authored-by: burgholzer <burgholzer@me.com>

--- docs/FigureOfMerit.rst
+++ docs/FigureOfMerit.rst
@@ -10,7 +10,8 @@ information needs to be collected, such as, e.g., gate/readout fidelities, gate 
 decoherence times. While for some devices, this information may be publicly available, for other
 devices it may be estimated from comparable devices, previous records, or insider knowledge.
 
-So far, two figures of merit are implemented: ``expected_fidelity`` and ``critical_depth``.
+So far, two figures of merit are implemented for all devices: ``expected_fidelity`` and ``critical_depth``.
+On top of that, the ``estimated_success_probability`` is available for devices where the necessary calibration data is available.
 
 Expected Fidelity
 -----------------
@@ -34,3 +35,15 @@ Critical Depth
 --------------
 A potential alternative could be the ``critical depth`` (taken from :cite:labelpar:`tomesh2022supermarq`)---a measure to describe the percentage of multi-qubit gates on the longest path through a compiled quantum circuit (determining the depth).
 A respective value close to 1 would indicate a very sequential circuit while a value of 0 would indicate a highly parallel one.
+
+
+Estimated Success Probability
+-----------------------------
+The ``estimated_success_probability`` (based on :cite:labelpar:`esp-lifetime-min`, :cite:labelpar:`esp-lifetime`, and :cite:labelpar:`esp-idle`) is a figure of merit that is based on the ``expected_fidelity`` but also multiplies it with a factor that considers the decoherence times :math:`T_1, T_2` of a device:
+
+.. math::
+   \prod_{q} \exp{(t_{q}^{\mathrm{idle}}/\min{(T_1, T_2)})}
+
+with :math:`t_{q}^{\mathrm{idle}}` being the sum of each qubit's idle times.
+Therefore, exactly the execution times of all gates and the decoherence times must be available.
+Note that some variants of this figure of merit do not take the minimum of both decoherence times but create one exponential factor for each decoherence time, others consider the entire qubit lifetime instead of idle times only.
--- tests/compilation/test_helper_rl.py
+++ tests/compilation/test_helper_rl.py
@@ -1,76 +1,76 @@
 """Tests for the helper functions of the reinforcement learning predictor."""
-#
-# from __future__ import annotations
-#
-# from pathlib import Path
-#
-# import numpy as np
-# from qiskit import transpile
-# from qiskit.transpiler import PassManager
-# from qiskit.transpiler.passes.layout.vf2_post_layout import VF2PostLayoutStopReason
-#
-# from mqt.bench import get_benchmark
-# from mqt.bench.devices import get_device_by_name
-# from mqt.predictor import rl
-#
-#
-# def test_create_feature_dict() -> None:
-#     """Test the creation of a feature dictionary."""
-#     qc = get_benchmark("dj", 1, 5)
-#     features = rl.helper.create_feature_dict(qc)
-#     for feature in features.values():
-#         assert isinstance(feature, np.ndarray | int)
-#
-#
-# def test_get_path_trained_model() -> None:
-#     """Test the retrieval of the path to the trained model."""
-#     path = rl.helper.get_path_trained_model()
-#     assert path.exists()
-#     assert isinstance(path, Path)
-#
-#
-# def test_get_path_training_circuits() -> None:
-#     """Test the retrieval of the path to the training circuits."""
-#     path = rl.helper.get_path_training_circuits()
-#     assert path.exists()
-#     assert isinstance(path, Path)
-#
-#
-# def test_vf2_layout_and_postlayout() -> None:
-#     """Test the VF2Layout and VF2PostLayout passes."""
-#     qc = get_benchmark("ghz", 1, 3)
-#
-#     for dev in [get_device_by_name("ibm_montreal"), get_device_by_name("ionq_harmony")]:
-#         layout_pass = None
-#         for layout_action in rl.helper.get_actions_layout():
-#             if layout_action["name"] == "VF2Layout":
-#                 layout_pass = layout_action["transpile_pass"](dev)
-#                 break
-#         pm = PassManager(layout_pass)
-#         layouted_qc = pm.run(qc)
-#         assert len(layouted_qc.layout.initial_layout) == dev.num_qubits
-#
-#     dev_success = get_device_by_name("ibm_montreal")
-#     qc_transpiled = transpile(
-#         qc, basis_gates=dev_success.basis_gates, coupling_map=dev_success.coupling_map, optimization_level=0
-#     )
-#     assert qc_transpiled.layout is not None
-#
-#     initial_layout_before = qc_transpiled.layout.initial_layout
-#
-#     post_layout_pass = None
-#     for layout_action in rl.helper.get_actions_final_optimization():
-#         if layout_action["name"] == "VF2PostLayout":
-#             post_layout_pass = layout_action["transpile_pass"](dev_success)
-#             break
-#
-#     pm = PassManager(post_layout_pass)
-#     altered_qc = pm.run(qc_transpiled)
-#
-#     assert pm.property_set["VF2PostLayout_stop_reason"] == VF2PostLayoutStopReason.SOLUTION_FOUND
-#
-#     postprocessed_vf2postlayout_qc, _ = rl.helper.postprocess_vf2postlayout(
-#         altered_qc, pm.property_set["post_layout"], qc_transpiled.layout
-#     )
-#
-#     assert initial_layout_before != postprocessed_vf2postlayout_qc.layout.initial_layout
+
+from __future__ import annotations
+
+from pathlib import Path
+
+import numpy as np
+from qiskit import transpile
+from qiskit.transpiler import PassManager
+from qiskit.transpiler.passes.layout.vf2_post_layout import VF2PostLayoutStopReason
+
+from mqt.bench import get_benchmark
+from mqt.bench.devices import get_device_by_name
+from mqt.predictor import rl
+
+
+def test_create_feature_dict() -> None:
+    """Test the creation of a feature dictionary."""
+    qc = get_benchmark("dj", 1, 5)
+    features = rl.helper.create_feature_dict(qc)
+    for feature in features.values():
+        assert isinstance(feature, np.ndarray | int)
+
+
+def test_get_path_trained_model() -> None:
+    """Test the retrieval of the path to the trained model."""
+    path = rl.helper.get_path_trained_model()
+    assert path.exists()
+    assert isinstance(path, Path)
+
+
+def test_get_path_training_circuits() -> None:
+    """Test the retrieval of the path to the training circuits."""
+    path = rl.helper.get_path_training_circuits()
+    assert path.exists()
+    assert isinstance(path, Path)
+
+
+def test_vf2_layout_and_postlayout() -> None:
+    """Test the VF2Layout and VF2PostLayout passes."""
+    qc = get_benchmark("ghz", 1, 3)
+
+    for dev in [get_device_by_name("ibm_montreal"), get_device_by_name("ionq_harmony")]:
+        layout_pass = None
+        for layout_action in rl.helper.get_actions_layout():
+            if layout_action["name"] == "VF2Layout":
+                layout_pass = layout_action["transpile_pass"](dev)
+                break
+        pm = PassManager(layout_pass)
+        layouted_qc = pm.run(qc)
+        assert len(layouted_qc.layout.initial_layout) == dev.num_qubits
+
+    dev_success = get_device_by_name("ibm_montreal")
+    qc_transpiled = transpile(
+        qc, basis_gates=dev_success.basis_gates, coupling_map=dev_success.coupling_map, optimization_level=0
+    )
+    assert qc_transpiled.layout is not None
+
+    initial_layout_before = qc_transpiled.layout.initial_layout
+
+    post_layout_pass = None
+    for layout_action in rl.helper.get_actions_final_optimization():
+        if layout_action["name"] == "VF2PostLayout":
+            post_layout_pass = layout_action["transpile_pass"](dev_success)
+            break
+
+    pm = PassManager(post_layout_pass)
+    altered_qc = pm.run(qc_transpiled)
+
+    assert pm.property_set["VF2PostLayout_stop_reason"] == VF2PostLayoutStopReason.SOLUTION_FOUND
+
+    postprocessed_vf2postlayout_qc, _ = rl.helper.postprocess_vf2postlayout(
+        altered_qc, pm.property_set["post_layout"], qc_transpiled.layout
+    )
+
+    assert initial_layout_before != postprocessed_vf2postlayout_qc.layout.initial_layout
